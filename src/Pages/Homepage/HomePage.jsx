02e64f8d955f0943a875c2ceb987efabbe5448ba
Ketan Khot <deltacodingketan@gmail.com>
2024-11-08 15:01:23 +0530

    Broken our homepage into smaller components and finished styling our menu components

---
+++ src/Pages/Homepage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HomePage.Style.scss";
+import Directory from "../../Components/Directory/Directory";
+
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
